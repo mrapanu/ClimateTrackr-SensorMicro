6f72847201f5be1a17dd0c76d1273bd15c600827
Mihai Râpanu <mrapanu@gmail.com>
2024-02-28 10:09:37 +0200

    handle the transition between days / month when calculating local time (23:59 - 00:00)

--- main.py
+++ main.py
@@ -44,10 +44,39 @@ def connect_wifi(ssid, password):
     except OSError as e:
         print('Failed to synchronize time with NTP server:', e)
 
+def days_in_month(year, month):
+    if month == 2:
+        if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+            return 29  # Leap year
+        else:
+            return 28
+    elif month in {4, 6, 9, 11}:
+        return 30
+    else:
+        return 31
+
 def get_local_time(gmt):
     utc_time = utime.localtime()
     local_hour = (utc_time[3] + gmt) % 24  # Ensure hour remains within 0-23 range
-    local_time = (utc_time[0], utc_time[1], utc_time[2], local_hour, utc_time[4], utc_time[5])
+    local_day = utc_time[2] + (utc_time[3] + gmt) // 24  # Adjust day if hour goes beyond 23 or below 0
+    local_month = utc_time[1]
+    local_year = utc_time[0]
+    
+    while local_day > days_in_month(local_year, local_month) or local_day < 1:
+        if local_day > days_in_month(local_year, local_month):
+            local_day -= days_in_month(local_year, local_month)
+            local_month += 1
+            if local_month > 12:
+                local_month = 1
+                local_year += 1
+        elif local_day < 1:
+            local_month -= 1
+            if local_month < 1:
+                local_month = 12
+                local_year -= 1
+            local_day += days_in_month(local_year, local_month)
+    
+    local_time = (local_year, local_month, local_day, local_hour, utc_time[4], utc_time[5])
     return local_time
 
 def read_dht_sensor():
